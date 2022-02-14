ae7e034f7c88389477e95062075c0964258cdeb8
Fernando Cavaccini <fernandocavaccini@gmail.com>
2022-02-14 14:28:48 -0300

    Integrating Worker Service with MySQL Database

--- worker-service/src/main/resources/db/migration/V2__Populate_Table_Worker_Service.sql
+++ worker-service/src/main/resources/db/migration/V2__Populate_Table_Worker_Service.sql
@@ -1,3 +1,3 @@
-INSERT INTO worker_service(name, daily_Income) VALUES('Josevaldo Ramos', 350.0);
-INSERT INTO worker_service(name, daily_Income) VALUES('Persival Alfredo', 250.0);
-INSERT INTO worker_service(name, daily_Income) VALUES('Serafino Mendes', 300.0);
+INSERT INTO worker_service (name, daily_income) VALUES('Josevaldo Ramos', 350.0);
+INSERT INTO worker_service (name, daily_income) VALUES('Persival Alfredo', 250.0);
+INSERT INTO worker_service (name, daily_income) VALUES('Serafino Mendes', 300.0);
