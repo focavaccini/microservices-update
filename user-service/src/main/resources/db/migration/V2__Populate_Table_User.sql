205fe25ea9e0a05b5adfb4ae0641fb78364a94df
Fernando Cavaccini <fernandocavaccini@gmail.com>
2022-02-18 11:25:19 -0300

    Integrating User service with MySQL Database

---
+++ user-service/src/main/resources/db/migration/V2__Populate_Table_User.sql
@@ -0,0 +1,2 @@
+INSERT INTO user (name, email, password) VALUES ('Nina Brown', 'nina@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+INSERT INTO user (name, email, password) VALUES ('Leia Red', 'leia@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
