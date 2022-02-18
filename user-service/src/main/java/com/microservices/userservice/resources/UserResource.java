b2e9e92de3fc863d27507d242890d3cb7990a47f
Fernando Cavaccini <fernandocavaccini@gmail.com>
2022-02-18 14:16:42 -0300

    Configuring Swagger and SpringDoc OpenAPI UI (Swagger) user-service

--- user-service/src/main/java/com/microservices/userservice/resources/UserResource.java
+++ user-service/src/main/java/com/microservices/userservice/resources/UserResource.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 import com.microservices.userservice.entities.User;
 import com.microservices.userservice.repositories.UserRepository;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "User endpoint")
 @RestController
 @RequestMapping("/user-service")
 public class UserResource {
@@ -18,12 +22,14 @@ public class UserResource {
 	@Autowired
 	private UserRepository repository;
 	
+	@Operation(summary = "Find user by id")
 	@GetMapping(value = "/{id}")
 	ResponseEntity<User> findById(@PathVariable Long id){
 		User obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}
 	
+	@Operation(summary = "Find user by email")
 	@GetMapping(value = "/search")
 	ResponseEntity<User> findByEmail(@RequestParam String email){
 		User obj = repository.findByEmail(email);
